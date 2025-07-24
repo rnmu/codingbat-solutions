2f51be25175ee4a3fdb896a2b29ade10d0dc8e75
rayan <rayanalhashmi8@gmail.com>
2025-07-24 09:22:04 +0400

    Add solution for warmup1-sleepIn

---
+++ warmup1/SleepIn.java
@@ -0,0 +1,11 @@
+public boolean sleepIn(boolean weekday, boolean vacation) {
+    if( weekday == false && vacation == false){
+        return true;
+    }else if( weekday == true && vacation == false){
+        return false;
+    }else if( weekday == false && vacation == true){
+        return true;
+    }else {
+        return true;
+    }
+}
