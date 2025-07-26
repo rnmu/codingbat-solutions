9a64405ac89d52b631151bcc78e0a038fa241682
rayan <rayanalhashmi8@gmail.com>
2025-07-26 17:19:42 +0400

    Add solution for warmup1-startOz

---
+++ warmup1/StartOz.java
@@ -0,0 +1,11 @@
+public String startOz(String str) {
+    if(str.length() > 1 && str.startsWith("oz") ){
+        return "oz";
+    }else if (str.length() > 0 && str.startsWith("o")) {
+        return "o";
+    }else if (str.length() > 0 && str.charAt(1)== 'z'){
+        return "z";
+    }else{
+        return "";
+    }
+}
