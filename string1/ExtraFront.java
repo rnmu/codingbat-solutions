2cfbc942518798260045b5428031eebbfc468193
rayan <rayanalhashmi8@gmail.com>
2025-07-27 20:56:01 +0400

    Add solution for string1-extraFront

---
+++ string1/ExtraFront.java
@@ -0,0 +1,10 @@
+public class ExtraFront {
+    public String extraFront(String str) {
+        if(str.length()<2){
+            return str + str + str;
+        }else{
+            String front = str.substring(0,2);
+            return  front + front + front;
+        }
+    }
+}
