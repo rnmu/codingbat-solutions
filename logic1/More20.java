3f8098f2944d7e0e180e1ee012deb4c5fb8054fa
rayan <rayanalhashmi8@gmail.com>
2025-07-28 13:06:40 +0400

    Add solution for logic1-more20

---
+++ logic1/More20.java
@@ -0,0 +1,9 @@
+public class More20 {
+    public boolean more20(int n) {
+        if(n% 20 == 1 ||n% 20 == 2 ){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
