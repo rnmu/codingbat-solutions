54ff1bf663c8c281511fdb1e97d11f6a7b034a26
rayan <rayanalhashmi8@gmail.com>
2025-07-28 13:17:45 +0400

    Add solution for logic1-less20

---
+++ logic1/Less20.java
@@ -0,0 +1,9 @@
+public class Less20 {
+    public boolean less20(int n) {
+        if(n % 20 == 18 || n % 20 == 19 ){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
