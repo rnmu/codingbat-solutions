30bba5c83dac98fab31f68d584699d02b5210db0
rayan <rayanalhashmi8@gmail.com>
2025-07-28 16:18:12 +0400

    Add solution for logic1-shareDigit

---
+++ logic1/ShareDigit.java
@@ -0,0 +1,9 @@
+public class ShareDigit {
+    public boolean shareDigit(int a, int b) {
+        if(a/10 == b/10 || a/10 == b%10 || a%10 == b/10 || a%10 == b%10){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
