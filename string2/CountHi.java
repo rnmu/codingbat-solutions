ef304be077e00df73846ea6d1e3951b1c0ecaa2d
rayan <rayanalhashmi8@gmail.com>
2025-07-28 23:52:13 +0400

    Add solution for string2-countHi

---
+++ string2/CountHi.java
@@ -0,0 +1,11 @@
+public class CountHi {
+    public int countHi(String str) {
+        int count = 0;
+        for(int i= 0 ; i <str.length()-1 ; i++){
+            if(str.substring(i,i+2).equals("hi")){
+                count +=1;
+            }
+        }
+        return count;
+    }
+}
