4840089134d45e1b4669be784d1c634aaab28f82
rayan <rayanalhashmi8@gmail.com>
2025-07-29 11:55:15 +0400

    Add solution for string2-repeatFront

---
+++ string2/RepeatFront.java
@@ -0,0 +1,9 @@
+public class RepeatFront {
+    public String repeatFront(String str, int n) {
+        String result = "";
+        for(int i = 0 ; i <n;i++){
+            result += str.substring(0,n-i) ;
+        }
+        return result;
+    }
+}
