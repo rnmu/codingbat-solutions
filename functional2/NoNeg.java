3f7eafa217a463390396d35603c89964d265604d
rayan <rayanalhashmi8@gmail.com>
2025-07-29 12:46:20 +0400

    Add solution for functional2-noNeg

---
+++ functional2/NoNeg.java
@@ -0,0 +1,7 @@
+public class NoNeg {
+    public List<Integer> noNeg(List<Integer> nums) {
+        return nums.stream()
+                .filter(n -> n >= 0)
+                .collect(Collectors.toList());
+    }
+}
