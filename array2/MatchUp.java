0a9e732184cfc873a1ad753431fd251f40070fe1
rayan <rayanalhashmi8@gmail.com>
2025-07-29 20:38:52 +0400

    Add solution for array2-matchUp

---
+++ array2/MatchUp.java
@@ -0,0 +1,11 @@
+public class MatchUp {
+    public int matchUp(int[] nums1, int[] nums2) {
+        int count = 0;
+        for(int i = 0; i< nums1.length;i++){
+            if(nums1[i] != nums2[i] && Math.abs(nums1[i]- nums2[i]) <= 2){
+                count ++;
+            }
+        }
+        return count;
+    }
+}
