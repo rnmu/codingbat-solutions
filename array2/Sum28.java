4db062272da556e7a3afbdacbaaaacee5c813711
rayan <rayanalhashmi8@gmail.com>
2025-07-29 18:19:44 +0400

    Add solution for array2-sum28

---
+++ array2/Sum28.java
@@ -0,0 +1,15 @@
+public class Sum28 {
+    public boolean sum28(int[] nums) {
+        int sum  = 0;
+        for(int i = 0 ;i <nums.length ;i++){
+            if(nums[i] == 2){
+                sum += nums[i];
+            }
+        }
+        if(sum == 8){
+            return true;
+        }else{
+            return false;
+        }
+    }
+}
