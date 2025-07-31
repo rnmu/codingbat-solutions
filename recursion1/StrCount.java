f25abf52b0bad2d15c09f192b1d2b43e87257b34
rayan <rayanalhashmi8@gmail.com>
2025-07-31 13:07:36 +0400

    Add solution for recursion1-strCount

---
+++ recursion1/StrCount.java
@@ -0,0 +1,12 @@
+public class StrCount {
+    public int strCount(String str, String sub) {
+        if(str.length() < sub.length()){
+            return 0;
+        }
+
+        if(str.startsWith(sub)){
+            return 1 + strCount(str.substring(sub.length()),sub);
+        }
+        return strCount(str.substring(1),sub);
+    }
+}
