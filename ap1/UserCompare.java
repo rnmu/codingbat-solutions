a52a7d3938ecaf938c5e0b790b7c86d99b8c7ad8
rayan <rayanalhashmi8@gmail.com>
2025-07-31 18:35:01 +0400

    Add solution for ap1-userCompare

---
+++ ap1/UserCompare.java
@@ -0,0 +1,17 @@
+public class UserCompare {
+    public int userCompare(String aName, int aId, String bName, int bId) {
+        if(aName.compareTo(bName) <0){
+            return -1;
+        }else if(aName.compareTo(bName) > 0){
+            return 1;
+        }else{
+            if ( aId < bId){
+                return -1;
+            }else if ( aId > bId){
+                return 1;
+            }else{
+                return 0;
+            }
+        }
+    }
+}
