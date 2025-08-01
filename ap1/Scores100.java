70d55147aecf1ee87eedb3c96558b337e26c9a9a
rayan <rayanalhashmi8@gmail.com>
2025-08-01 08:37:23 +0400

    Add solution for ap1-scores100

---
+++ ap1/Scores100.java
@@ -0,0 +1,10 @@
+public class Scores100 {
+    public boolean scores100(int[] scores) {
+        for(int i = 0; i<scores.length-1 ;i++){
+            if(scores[i] == 100 && scores[i+1] == 100 ){
+                return true;
+            }
+        }
+        return false;
+    }
+}
