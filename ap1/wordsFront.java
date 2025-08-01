a920acf7b0f6901c4ccb8341a6f0aa54f073a035
rayan <rayanalhashmi8@gmail.com>
2025-08-01 14:47:01 +0400

    Add solution for ap1-wordsFront

---
+++ ap1/wordsFront.java
@@ -0,0 +1,9 @@
+public class wordsFront {
+    public String[] wordsFront(String[] words, int n) {
+        String [] result = new String [n];
+        for(int i=0 ;i<n ;i++){
+            result[i] = words[i];
+        }
+        return result;
+    }
+}
