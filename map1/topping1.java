7a1396f99946b3ea006585e720a533f1e001ff4a
rayan <rayanalhashmi8@gmail.com>
2025-08-02 01:25:00 +0400

    Add solution for map1-topping1

---
+++ map1/topping1.java
@@ -0,0 +1,10 @@
+public class topping1 {
+    public Map<String, String> topping1(Map<String, String> map) {
+        map.put("bread","butter");
+        if(map.containsKey("ice cream")){
+            map.put("ice cream","cherry");
+        }
+
+        return map;
+    }
+}
