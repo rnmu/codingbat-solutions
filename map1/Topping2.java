3e6af1da664ddb8d170aa2fe26b3fd34770e7c20
rayan <rayanalhashmi8@gmail.com>
2025-08-02 09:34:48 +0400

    Add solution for map1-topping2

---
+++ map1/Topping2.java
@@ -0,0 +1,12 @@
+public class Topping2 {
+    public Map<String, String> topping2(Map<String, String> map) {
+        if(map.containsKey("ice cream")){
+            map.put("yogurt", map.get("ice cream"));
+        }
+        if(map.containsKey("spinach")){
+            map.put("spinach", "nuts");
+        }
+
+        return map;
+    }
+}
